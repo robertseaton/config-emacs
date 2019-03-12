e039ba84c92e1fe954589e2d4fd726fb60750805
Robert Seaton <robbpseaton@gmail.com>
2019-03-11 21:46:07 -0500

    Typo.

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
 (setq package-list
-    '(f use-package markdown-mode nixos-mode magit))
+    '(f use-package markdown-mode nix-mode magit))
 
 ; activate all the packages
 (package-initialize)
